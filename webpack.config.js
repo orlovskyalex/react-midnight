07d4e1728f3b11492d24e902e131f66233cdbf39
Aleksei Kaida <alex94orlovsky@gmail.com>
2017-08-28 23:35:25 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: {
+		index: './src/js/index.js',
+		styles: './src/assets/scss/root.scss',
+	},
+	output: {
+		publicPath: '/',
+		path: path.resolve('dist'),
+		filename: '[name]_[hash].bundle.js',
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.s?css$/,
+				loader: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader!resolve-url-loader!sass-loader?sourceMap',
+				}),
+			},
+			{
+				test: /\.html$/,
+				loader: 'html-loader',
+			},
+		],
+	},
+	plugins: [
+		new ExtractTextPlugin({
+			filename: 'style.[contenthash].css',
+			allChunks: true,
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			inject: 'body',
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+	],
+	devServer: {
+		port: 8080,
+		compress: true,
+		hot: true,
+		inline: true,
+		stats: 'errors-only',
+		historyApiFallback: true,
+	},
+};
